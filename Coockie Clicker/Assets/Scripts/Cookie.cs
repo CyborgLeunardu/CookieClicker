98194cccb98ee1dec013190654338954fcc69740
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-10-17 15:49:23 -0300

    Script do Cookie
    Adicionado migalha ao clicar no cookie

---
+++ Coockie Clicker/Assets/Scripts/Cookie.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Cookie : MonoBehaviour
+{
+    public GameObject Migalhas;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Coockie Clicker/Assets/Scripts/GameManager.cs
+++ Coockie Clicker/Assets/Scripts/GameManager.cs
@@ -8,20 +8,30 @@ public class GameManager : MonoBehaviour
     public Transform cookiePosicao;
     public TextMeshProUGUI cookiesComidosTexto;
     public float cookiesComidos;
+    public Cookie cookieGerado;
+    public Transform migalhaPosicao;
 
     //Instancia os objetos criados
     public void GerarCookie(GameObject cookie)
     {
-        Instantiate(cookie, cookiePosicao.position, cookiePosicao.rotation);
+        //Não é possível converter UnityEngine.GameObject para Cookie
+        //cookieGerado = Instantiate(cookie, cookiePosicao.position, cookiePosicao.rotation);
+
+        //Resolução:
+        cookieGerado = Instantiate(cookie, cookiePosicao.position, cookiePosicao.rotation).GetComponent<Cookie>();
+
     }
 
+    //Método para contar número de clicks
     public void ClicouNoCookie()
     {
         cookiesComidos++;
         cookiesComidosTexto.text = cookiesComidos.ToString();
+        //Instanciar a migalha toda vez que clickar no cookie
+        Instantiate(cookieGerado.Migalhas, migalhaPosicao.position, migalhaPosicao.localRotation);
     }
 
-    //Passar o método no Start
+    //Passar o método Gerar e atualizando o valor dos clicks do método ClicouNoCookie
     void Start()
     {
         cookiesComidos = 0;
